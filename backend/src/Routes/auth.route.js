c3b66c4aab9aab72b4d2aeb7d9dfa0f66fbeaeec
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2026-06-17 19:16:19 +0530

    Fixed invalid import

--- backend/src/Routes/auth.route.js
+++ backend/src/Routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, logout, refreshAccessToken, signup, verifyEmail } from "../Controllers/user.controller.js";
+import { login, logout, refreshAccessToken, signup, verifyEmail } from "../Controllers/auth.controller.js";
 import { verifyJWT } from "../Middlewares/auth.middleware.js";
 
 const authRouter = Router()
